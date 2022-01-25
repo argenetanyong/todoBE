20d6ae89332f6d746caf2cc02cc45b0b137714f5
Argene <argene.tanyong@gmail.com>
2022-01-25 15:14:51 +0800

    Change driver version in MongoDB

--- index.js
+++ index.js
@@ -10,18 +10,20 @@ Mongodb connection local
 mongoose
   .connect("mongodb://localhost/todos")
   .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...")); */
+  .catch((err) => console.error("Could not connect to MongoDB..."));
+*/
 
 // mongodb connection string should be set in the environment variable
 // The hardcoded connection string is for portfolio/demo purpose only
 /* mongoose
   .connect("mongodb+srv://logan:14344@cluster0.mfkia.mongodb.net/todoBE")
   .then(() => console.log("Connected to REMOTE MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...")); */
+  .catch((err) => console.error("Could not connect to MongoDB..."));
+*/
 
 mongoose
   .connect(
-    "mongodb+srv://logan:14344@cluster0.mfkia.mongodb.net/todoBE?retryWrites=true&w=majority"
+    "mongodb://logan:14344@cluster0-shard-00-00.mfkia.mongodb.net:27017,cluster0-shard-00-01.mfkia.mongodb.net:27017,cluster0-shard-00-02.mfkia.mongodb.net:27017/todoBE?ssl=true&replicaSet=atlas-cvlux3-shard-0&authSource=admin&retryWrites=true&w=majority"
   )
   .then(() => console.log("Connected to THE REMOTE MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
