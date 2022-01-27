575ad435c64bf0306a949be60e4231833590f667
Argene <argene.tanyong@gmail.com>
2022-01-27 18:59:38 +0800

    Removed Helmet and COmpression middlewares. Added https://enable-cors.org/ instruction

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const mongoose = require("mongoose");
 const todos = require("./routes/todos");
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const compression = require("compression");
-const helmet = require("helmet");
 
 /*
 
@@ -28,9 +25,15 @@ mongoose
 
 app.use(express.json());
 app.use("/api/todos", todos);
-app.use(compression());
-app.use(helmet());
-app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
